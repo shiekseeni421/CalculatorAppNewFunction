259c8634f5f8b525e1a10c6ce6b17c2cabb01655
shiekseeni421 <61022287+shiekseeni421@users.noreply.github.com>
2022-04-26 18:39:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import CalculatorApp from "./Component/CalculatorApp";
+function App() {
+  return (
+    <div className="App">
+      <CalculatorApp />
+    </div>
+  );
+}
+
+export default App;
